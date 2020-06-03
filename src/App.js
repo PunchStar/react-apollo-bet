9f9d0a3ad75db53e4705eb0a9491d8c8b50bfc80
PunchStar <superpunch727@gmail.com>
2020-06-03 22:15:40 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloProvider } from 'react-apollo';
+import Dashboard from './pages/Dashboard'
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://gambilife.com/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Dashboard/>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <div className="navbar navbar-fixed">
+      <div className="navbar-right" />
+    </div>
+  );
+};
+
+export default Header;
